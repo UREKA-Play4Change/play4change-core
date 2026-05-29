578e4a73321b0a13e517802877abe2baf953ccb9
Radesh Govind <radesh.govind@gmail.com>
2026-05-29 17:05:44 +0100

    feat(mobile): add ExploreFilter enum + SetFilter event + filter state

--- composeApp/src/commonMain/kotlin/com/ureka/play4change/features/explore/presentation/ExploreEvents.kt
+++ composeApp/src/commonMain/kotlin/com/ureka/play4change/features/explore/presentation/ExploreEvents.kt
@@ -5,6 +5,7 @@ import com.ureka.play4change.features.explore.domain.model.Topic
 
 sealed interface ExploreEvents : ComponentEvents {
     data object LoadTopics : ExploreEvents
+    data class SetFilter(val filter: ExploreFilter) : ExploreEvents
     data class RequestEnroll(val topic: Topic) : ExploreEvents
     data object ConfirmEnroll : ExploreEvents
     data object DismissEnroll : ExploreEvents
---
+++ composeApp/src/commonMain/kotlin/com/ureka/play4change/features/explore/presentation/ExploreFilter.kt
@@ -0,0 +1,3 @@
+package com.ureka.play4change.features.explore.presentation
+
+enum class ExploreFilter { ALL, ACTIVE, OPEN, COMPLETED, ABANDONED, LOCKED }
