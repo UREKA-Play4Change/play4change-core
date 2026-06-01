ae0ec828de1e01fe165371cbd3710bdb189206da
Radesh Govind <radesh.govind@gmail.com>
2026-06-01 13:58:05 +0100

    feat(android): register AndroidSocialAuthLauncher in Koin platformModule

--- composeApp/src/androidMain/kotlin/com/ureka/play4change/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/ureka/play4change/di/PlatformModule.android.kt
@@ -4,6 +4,8 @@ import com.ureka.play4change.BuildConfig
 import com.ureka.play4change.core.network.EncryptedSharedPreferencesTokenStorage
 import com.ureka.play4change.core.network.NetworkConfig
 import com.ureka.play4change.core.network.TokenStorage
+import com.ureka.play4change.features.auth.platform.AndroidSocialAuthLauncher
+import com.ureka.play4change.features.auth.platform.SocialAuthLauncher
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -11,4 +13,5 @@ import org.koin.dsl.module
 actual val platformModule: Module = module {
     single { NetworkConfig(BuildConfig.BASE_URL, BuildConfig.USE_MOCKS) }
     single<TokenStorage> { EncryptedSharedPreferencesTokenStorage(androidContext()) }
+    single<SocialAuthLauncher> { AndroidSocialAuthLauncher(BuildConfig.GOOGLE_WEB_CLIENT_ID) }
 }
