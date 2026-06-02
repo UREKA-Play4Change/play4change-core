df48c48301eb6221ba69df7e4620d1e3f144b478
Radesh Govind <radesh.govind@gmail.com>
2026-06-02 10:03:22 +0100

    remove SocialAuthLauncher binding from AuthModule

--- composeApp/src/commonMain/kotlin/com/ureka/play4change/di/features/AuthModule.kt
+++ composeApp/src/commonMain/kotlin/com/ureka/play4change/di/features/AuthModule.kt
@@ -4,7 +4,6 @@ import com.ureka.play4change.core.network.NetworkConfig
 import com.ureka.play4change.features.auth.data.http.HttpAuthRepository
 import com.ureka.play4change.features.auth.data.mock.MockAuthRepository
 import com.ureka.play4change.features.auth.domain.repository.AuthRepository
-import com.ureka.play4change.features.auth.platform.SocialAuthLauncher
 import com.ureka.play4change.features.auth.presentation.DefaultLoginComponent
 import io.ktor.client.HttpClient
 import org.koin.dsl.module
@@ -16,6 +15,6 @@ val authModule = module {
         else HttpAuthRepository(get<HttpClient>(), get())
     }
     factory { (context: com.arkivanov.decompose.ComponentContext) ->
-        DefaultLoginComponent(context, get(), getOrNull<SocialAuthLauncher>())
+        DefaultLoginComponent(context, get())
     }
 }
