b98d1ac9f165bf073cc7af86b0324ab4c3bfe72a
Radesh Govind <radesh.govind@gmail.com>
2026-06-02 10:03:21 +0100

    remove socialLogin mock from MockAuthRepository

--- composeApp/src/commonMain/kotlin/com/ureka/play4change/features/auth/data/mock/MockAuthRepository.kt
+++ composeApp/src/commonMain/kotlin/com/ureka/play4change/features/auth/data/mock/MockAuthRepository.kt
@@ -3,7 +3,6 @@ package com.ureka.play4change.features.auth.data.mock
 import com.ureka.play4change.auth.AuthTokens
 import com.ureka.play4change.features.auth.domain.model.AuthResult
 import com.ureka.play4change.features.auth.domain.model.MagicLinkResult
-import com.ureka.play4change.features.auth.domain.model.SocialProvider
 import com.ureka.play4change.features.auth.domain.repository.AuthRepository
 import kotlinx.coroutines.delay
 
@@ -21,18 +20,6 @@ class MockAuthRepository : AuthRepository {
             tokens = AuthTokens(
                 accessToken = "mock-access-token",
                 refreshToken = "mock-refresh-token",
-                expiresIn = 900L
-            )
-        )
-    }
-
-    override suspend fun socialLogin(provider: SocialProvider, idToken: String): AuthResult {
-        delay(1200)
-        return AuthResult(
-            userId = "mock-social-user",
-            tokens = AuthTokens(
-                accessToken = "mock-social-access-token",
-                refreshToken = "mock-social-refresh-token",
                 expiresIn = 900L
             )
         )
