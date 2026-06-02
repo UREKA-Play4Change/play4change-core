c14ef36dda7348775f2ed6fd996a668e72b20774
Radesh Govind <radesh.govind@gmail.com>
2026-06-02 10:02:37 +0100

    remove GoogleProperties

--- server/src/main/kotlin/com/ureka/play4change/auth/application/GoogleProperties.kt
+++
@@ -1,8 +0,0 @@
-package com.ureka.play4change.auth.application
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "google.oauth")
-data class GoogleProperties(
-    val clientId: String = ""
-)
