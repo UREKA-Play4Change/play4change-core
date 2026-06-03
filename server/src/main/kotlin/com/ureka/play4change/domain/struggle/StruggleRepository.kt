2722d8ae9bbda2a599b257b5d5b69785ed7da6a2
Radesh Govind <radesh.govind@gmail.com>
2026-06-03 21:55:18 +0100

    feat(struggle): add findAllByEnrollmentId to StruggleRepository

--- server/src/main/kotlin/com/ureka/play4change/domain/struggle/StruggleRepository.kt
+++ server/src/main/kotlin/com/ureka/play4change/domain/struggle/StruggleRepository.kt
@@ -3,6 +3,7 @@ package com.ureka.play4change.domain.struggle
 interface StruggleRepository {
     fun findById(id: String): StruggleSession?
     fun findOpenByEnrollmentId(enrollmentId: String): StruggleSession?
+    fun findAllByEnrollmentId(enrollmentId: String): List<StruggleSession>
     fun save(session: StruggleSession): StruggleSession
     fun findAdaptiveTasksByTopicId(topicId: String): List<AdaptiveTaskAdminView>
     fun findPathStatsByTopicId(topicId: String): List<StrugglePathStats>
