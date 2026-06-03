8db54ffee652b2621c0fb7348a80a2105faf03e2
Radesh Govind <radesh.govind@gmail.com>
2026-06-03 20:56:12 +0100

    feat(admin): add getStrugglePathStats to AdminTaskUseCase port

--- server/src/main/kotlin/com/ureka/play4change/application/port/AdminTaskUseCase.kt
+++ server/src/main/kotlin/com/ureka/play4change/application/port/AdminTaskUseCase.kt
@@ -2,6 +2,7 @@ package com.ureka.play4change.application.port
 
 import arrow.core.Either
 import com.ureka.play4change.domain.struggle.AdaptiveTaskAdminView
+import com.ureka.play4change.domain.struggle.StrugglePathStats
 import com.ureka.play4change.domain.topic.TaskQuestionStats
 import com.ureka.play4change.domain.topic.TaskTemplate
 import com.ureka.play4change.error.AppError
@@ -22,6 +23,7 @@ data class UpdateTaskCommand(
 interface AdminTaskUseCase {
     fun getTasksForTopic(topicId: String): Either<AppError, List<TaskTemplateWithStats>>
     fun getStruggleTasksForTopic(topicId: String): Either<AppError, List<AdaptiveTaskAdminView>>
+    fun getStrugglePathStats(topicId: String): Either<AppError, List<StrugglePathStats>>
     fun updateTask(templateId: String, command: UpdateTaskCommand): Either<AppError, TaskTemplate>
     fun updateAdaptiveTask(taskId: String, command: UpdateTaskCommand): Either<AppError, AdaptiveTaskAdminView>
 }
