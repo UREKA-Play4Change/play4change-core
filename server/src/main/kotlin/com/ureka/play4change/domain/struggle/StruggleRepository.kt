1b9659412a66724738c68ecffd1e274187236b21
Radesh Govind <radesh.govind@gmail.com>
2026-06-03 20:56:03 +0100

    feat(struggle): add findPathStatsByTopicId to StruggleRepository

--- server/src/main/kotlin/com/ureka/play4change/domain/struggle/StruggleRepository.kt
+++ server/src/main/kotlin/com/ureka/play4change/domain/struggle/StruggleRepository.kt
@@ -5,6 +5,7 @@ interface StruggleRepository {
     fun findOpenByEnrollmentId(enrollmentId: String): StruggleSession?
     fun save(session: StruggleSession): StruggleSession
     fun findAdaptiveTasksByTopicId(topicId: String): List<AdaptiveTaskAdminView>
+    fun findPathStatsByTopicId(topicId: String): List<StrugglePathStats>
     fun findAdaptiveTaskById(taskId: String): AdaptiveTask?
     fun findAdaptiveTaskViewById(taskId: String): AdaptiveTaskAdminView?
     fun saveAdaptiveTask(task: AdaptiveTask): AdaptiveTask
