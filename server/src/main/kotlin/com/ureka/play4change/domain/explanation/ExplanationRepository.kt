3a58f1a0a8e80ba0d33205aec7b7db1c02b7cce0
Radesh Govind <radesh.govind@gmail.com>
2026-06-07 17:42:31 +0100

    feat(explanation): add findAllByEnrollmentId to ExplanationRepository interface

--- server/src/main/kotlin/com/ureka/play4change/domain/explanation/ExplanationRepository.kt
+++ server/src/main/kotlin/com/ureka/play4change/domain/explanation/ExplanationRepository.kt
@@ -3,6 +3,7 @@ package com.ureka.play4change.domain.explanation
 interface ExplanationRepository {
     fun findById(id: String): ExplanationSession?
     fun findActiveByEnrollmentId(enrollmentId: String): ExplanationSession?
+    fun findAllByEnrollmentId(enrollmentId: String): List<ExplanationSession>
     fun save(session: ExplanationSession): ExplanationSession
     fun saveMessage(message: ExplanationMessage): ExplanationMessage
     fun findMessagesBySessionId(sessionId: String): List<ExplanationMessage>
