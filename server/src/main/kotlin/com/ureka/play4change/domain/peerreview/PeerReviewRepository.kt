e9c03d600113a63df18f8df3a8a8372ffc088c58
Radesh Govind <radesh.govind@gmail.com>
2026-06-07 21:03:00 +0100

    perf(peer-review): add batch findBySubmissionAssignmentIdIn to PeerReviewRepository

--- server/src/main/kotlin/com/ureka/play4change/domain/peerreview/PeerReviewRepository.kt
+++ server/src/main/kotlin/com/ureka/play4change/domain/peerreview/PeerReviewRepository.kt
@@ -3,6 +3,7 @@ package com.ureka.play4change.domain.peerreview
 interface PeerReviewRepository {
     fun findById(id: String): PeerReview?
     fun findBySubmissionAssignmentId(submissionAssignmentId: String): List<PeerReview>
+    fun findBySubmissionAssignmentIdIn(submissionAssignmentIds: List<String>): List<PeerReview>
     fun findPendingByReviewerUserId(reviewerUserId: String): List<PeerReview>
     fun findExpiredPending(now: java.time.OffsetDateTime): List<PeerReview>
     fun countBySubmissionAssignmentId(submissionAssignmentId: String): Int
