bb373511b40c0e527973d2063d856d9f8fadeb2f
Radesh Govind <radesh.govind@gmail.com>
2026-06-14 18:23:40 +0100

    refactor(server): remove findExpiredPending from PeerReviewRepository interface

--- server/src/main/kotlin/com/ureka/play4change/domain/peerreview/PeerReviewRepository.kt
+++ server/src/main/kotlin/com/ureka/play4change/domain/peerreview/PeerReviewRepository.kt
@@ -5,7 +5,6 @@ interface PeerReviewRepository {
     fun findBySubmissionAssignmentId(submissionAssignmentId: String): List<PeerReview>
     fun findBySubmissionAssignmentIdIn(submissionAssignmentIds: List<String>): List<PeerReview>
     fun findPendingByReviewerUserId(reviewerUserId: String): List<PeerReview>
-    fun findExpiredPending(now: java.time.OffsetDateTime): List<PeerReview>
     fun countBySubmissionAssignmentId(submissionAssignmentId: String): Int
     fun save(peerReview: PeerReview): PeerReview
     fun saveAll(reviews: List<PeerReview>): List<PeerReview>
