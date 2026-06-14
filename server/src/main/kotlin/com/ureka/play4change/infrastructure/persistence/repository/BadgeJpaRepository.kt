99b4a5343309c06f756d7d2f2784904396bf3f43
Radesh Govind <radesh.govind@gmail.com>
2026-06-14 18:05:39 +0100

    feat(server): add Pageable overload to BadgeJpaRepository

--- server/src/main/kotlin/com/ureka/play4change/infrastructure/persistence/repository/BadgeJpaRepository.kt
+++ server/src/main/kotlin/com/ureka/play4change/infrastructure/persistence/repository/BadgeJpaRepository.kt
@@ -1,6 +1,8 @@
 package com.ureka.play4change.infrastructure.persistence.repository
 
 import com.ureka.play4change.infrastructure.persistence.entity.BadgeEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
@@ -10,6 +12,7 @@ interface BadgeJpaRepository : JpaRepository<BadgeEntity, String> {
     fun findByUserIdAndMicroCompetenceId(userId: String, microCompetenceId: String): BadgeEntity?
 
     fun findByUserId(userId: String): List<BadgeEntity>
+    fun findByUserId(userId: String, pageable: Pageable): Page<BadgeEntity>
 
     @Query("SELECT b FROM BadgeEntity b WHERE b.microCompetence.id = :microCompetenceId")
     fun findByMicroCompetenceId(microCompetenceId: String): List<BadgeEntity>
