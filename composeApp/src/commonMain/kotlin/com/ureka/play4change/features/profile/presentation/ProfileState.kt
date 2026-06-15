344b6bfbe00447b1938b34da3061d2ff0ea7147c
Radesh Govind <radesh.govind@gmail.com>
2026-06-15 10:12:59 +0100

    fix(mobile): add badgePage field to ProfileState

--- composeApp/src/commonMain/kotlin/com/ureka/play4change/features/profile/presentation/ProfileState.kt
+++ composeApp/src/commonMain/kotlin/com/ureka/play4change/features/profile/presentation/ProfileState.kt
@@ -1,15 +1,16 @@
 package com.ureka.play4change.features.profile.presentation
 
 import com.ureka.play4change.core.component.base.ComponentState
-import com.ureka.play4change.core.error.AppError
+import com.ureka.play4change.core.error.UiError
 import com.ureka.play4change.features.profile.domain.model.ProfileData
 
 data class ProfileState(
     override val isLoading: Boolean = true,
-    override val error: AppError? = null,
+    override val error: UiError? = null,
     val profile: ProfileData? = null,
     val isEditingName: Boolean = false,
     val nameInput: String = "",
     val isSavingName: Boolean = false,
-    val languagePickerVisible: Boolean = false
+    val languagePickerVisible: Boolean = false,
+    val badgePage: Int = 0
 ) : ComponentState
