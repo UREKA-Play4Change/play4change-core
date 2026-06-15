73ab5ba4b53d6efdb4b901dd81d6acad2ecef494
Radesh Govind <radesh.govind@gmail.com>
2026-06-15 10:54:47 +0100

    refactor(application): remove GetUserProfileUseCase from user package — moved to application/port

--- server/src/main/kotlin/com/ureka/play4change/application/user/GetUserProfileUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.ureka.play4change.application.user
-
-import arrow.core.Either
-import com.ureka.play4change.error.AppError
-
-interface GetUserProfileUseCase {
-    fun execute(userId: String): Either<AppError, UserProfile>
-}
