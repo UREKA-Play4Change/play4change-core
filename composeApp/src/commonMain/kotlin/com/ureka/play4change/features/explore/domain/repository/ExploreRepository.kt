b8a4863ca5eda541f598698926e0a9103b957660
Radesh Govind <radesh.govind@gmail.com>
2026-06-15 10:12:27 +0100

    fix(mobile): change ExploreRepository.getTopics to accept page/size and return TopicPage

--- composeApp/src/commonMain/kotlin/com/ureka/play4change/features/explore/domain/repository/ExploreRepository.kt
+++ composeApp/src/commonMain/kotlin/com/ureka/play4change/features/explore/domain/repository/ExploreRepository.kt
@@ -1,9 +1,10 @@
 package com.ureka.play4change.features.explore.domain.repository
 
 import com.ureka.play4change.features.explore.domain.model.Topic
+import com.ureka.play4change.features.explore.domain.model.TopicPage
 
 interface ExploreRepository {
-    suspend fun getTopics(userId: String): List<Topic>
+    suspend fun getTopics(userId: String, page: Int, size: Int): TopicPage
     suspend fun enrollTopic(userId: String, topicId: String): Boolean
     suspend fun deactivateEnrollment(userId: String, topicId: String): Boolean
 }
