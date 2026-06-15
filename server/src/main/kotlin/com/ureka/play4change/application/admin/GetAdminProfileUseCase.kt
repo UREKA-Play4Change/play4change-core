dbe7e3fb0ea4b9e0ee6fe8804b5f1fac57727ab0
Radesh Govind <radesh.govind@gmail.com>
2026-06-15 10:54:44 +0100

    refactor(application): remove GetAdminProfileUseCase from admin package — moved to application/port

--- server/src/main/kotlin/com/ureka/play4change/application/admin/GetAdminProfileUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.ureka.play4change.application.admin
-
-import arrow.core.Either
-import com.ureka.play4change.error.AppError
-
-interface GetAdminProfileUseCase {
-    fun execute(adminId: String): Either<AppError, AdminProfile>
-}
