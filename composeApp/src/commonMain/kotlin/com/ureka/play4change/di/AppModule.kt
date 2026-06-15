a82405e91003fc46d8b9a27e1036a2bec32ad578
Radesh Govind <radesh.govind@gmail.com>
2026-06-15 10:57:07 +0100

    refactor(mobile): update AppModule — wire UiError-aware components through DI graph

--- composeApp/src/commonMain/kotlin/com/ureka/play4change/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/ureka/play4change/di/AppModule.kt
@@ -4,7 +4,6 @@ import com.ureka.play4change.di.features.aboutModule
 import com.ureka.play4change.di.features.authModule
 import com.ureka.play4change.di.features.exploreModule
 import com.ureka.play4change.di.features.homeModule
-import com.ureka.play4change.di.features.peerReviewModule
 import com.ureka.play4change.di.features.profileModule
 import com.ureka.play4change.di.features.splashModule
 import com.ureka.play4change.di.features.explanationModule
@@ -12,11 +11,12 @@ import com.ureka.play4change.di.features.struggleModule
 import com.ureka.play4change.di.features.taskModule
 import org.koin.dsl.module
 
+// TODO: re-add peerReviewModule once presentation/ and ui/ layers are implemented
 val appModule = module {
     includes(
         platformModule, coreModule,
         splashModule, authModule, homeModule, taskModule,
         profileModule, aboutModule, exploreModule,
-        struggleModule, peerReviewModule, explanationModule
+        struggleModule, explanationModule
     )
 }
