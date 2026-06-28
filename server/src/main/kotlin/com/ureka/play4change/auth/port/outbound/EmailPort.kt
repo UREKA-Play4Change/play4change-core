81b5cbc7d2b0ed74d71f760478f3780ecaf819bc
Radesh Govind <radesh.govind@gmail.com>
2026-06-28 12:22:38 +0100

    feat(server): add sendRecoveryEmailVerification to EmailPort

--- server/src/main/kotlin/com/ureka/play4change/auth/port/outbound/EmailPort.kt
+++ server/src/main/kotlin/com/ureka/play4change/auth/port/outbound/EmailPort.kt
@@ -2,4 +2,5 @@ package com.ureka.play4change.auth.port.outbound
 
 interface EmailPort {
     fun sendMagicLink(toEmail: String, token: String)
+    fun sendRecoveryEmailVerification(toEmail: String, token: String)
 }
